44dd42b519e3c4e45e02b5d636917ce755bb9276
Ronald de Haan <me@ronalddehaan.eu>
2023-12-18 12:45:33 +0100

    Add cegar mode

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -6,8 +6,20 @@ from collections import defaultdict
 from itertools import combinations
 import textwrap
 
+from typing import Sequence, List, Tuple, cast
+
 import clingo
 from clingox.reify import reify_program
+
+from clingo import ast
+from clingo.solving import SolveResult
+from clingo.ast import ProgramBuilder
+from clingo.control import Control
+from clingo.application import clingo_main, Application
+from clingo.propagator import PropagateControl, PropagateInit, Propagator
+from clingo.backend import Backend
+from clingo.ast import parse_string, parse_files, AST, ASTType
+from clingox.ast import rename_symbolic_atoms
 
 class RectangularPuzzle:
     """
@@ -174,11 +186,20 @@ class RectangularPuzzle:
         verbose=False,
         num_models=1,
         precompute_solution=False,
+        use_cegar=False,
         enforce_essential_constraints=True,
     ):
         """
         TODO
         """
+
+        if use_cegar:
+            self.generate_with_cegar(
+                verbose=verbose,
+                num_models=num_models,
+                enforce_essential_constraints=enforce_essential_constraints
+            )
+            return
 
         essential_solution_program = ""
         reified_solution_programs = []
@@ -218,7 +239,7 @@ class RectangularPuzzle:
             control = clingo.Control([
                 '--project',
                 '--warn=none',
-                '--parallel-mode=4',
+                '--parallel-mode=1',
                 '--heuristic=Domain',
             ])
             program = self.domain_program
@@ -289,16 +310,12 @@ class RectangularPuzzle:
 
                 :- alt(I), alt(I,conjunction(B)) :
                         alt(I,output(solution(C,V),B)), solution(C,V).
-
-                raar :- alt(I), alt(I,conjunction(B)) :
-                        alt(I,output(solution(C,V),B)), solution(C,V).
-                #show print(raar) : raar.
             """
 
         control = clingo.Control([
             '--project',
             '--warn=none',
-            '--parallel-mode=4',
+            '--parallel-mode=1',
             '--heuristic=Domain',
         ])
         control.load("metaD.lp")
@@ -329,6 +346,267 @@ class RectangularPuzzle:
         if verbose:
             time = control.statistics['summary']['times']['total']
             print(f"Solving time: {time:.2f} seconds\n")
+
+    def generate_with_cegar(
+        self,
+        verbose=False,
+        num_models=1,
+        enforce_essential_constraints=True,
+    ):
+        """
+        TODO
+        """
+
+        # TODO: implement precompute_solution
+        # TODO: figure out if specific glue option performs better
+        #   (i.e., only glue is solution/2 and puzzle/2)
+
+        control = clingo.Control([
+            '--project',
+            '--warn=none',
+            '--parallel-mode=1',
+            '--heuristic=Domain',
+        ])
+
+        essential_solution_program = ""
+        reified_solution_programs = []
+        if self.essential_solution_constraints:
+            essential_solution_program = "".join(
+                self.essential_solution_constraints
+            )
+
+            if enforce_essential_constraints:
+                for i, solution_constraints in enumerate(list(map(list,
+                    combinations(
+                        self.essential_solution_constraints,
+                        r=len(self.essential_solution_constraints)-1)
+                    )
+                )):
+                    program_to_reify = self.domain_program
+                    program_to_reify += self.puzzle_gen_program
+                    program_to_reify += self.solution_program
+                    program_to_reify += "".join(solution_constraints)
+
+                    reified_symbols = reify_program(
+                        program_to_reify,
+                        calculate_sccs=True,
+                    )
+                    reified_program = "".join([
+                        f"alt({i},{symbol}).\n"
+                        for symbol in reified_symbols
+                    ])
+                    reified_solution_programs.append(reified_program)
+
+        if reified_solution_programs:
+            essential_solution_program += "".join(
+                reified_solution_programs
+            )
+
+        glue_program = ""
+        if reified_solution_programs:
+            glue_program += """
+                :- puzzle(C,V),
+                    alt(I,output(puzzle(C,V),B)), not alt(I,conjunction(B)).
+                :- not puzzle(C,V),
+                    alt(I,output(puzzle(C,V),B)), alt(I,conjunction(B)).
+
+                :- guessed_number(Name,Num),
+                    alt(I,output(guessed_number(Name,Num),B)),
+                    not alt(I,conjunction(B)).
+                :- not guessed_number(Name,Num),
+                    alt(I,output(guessed_number(Name,Num),B)),
+                    alt(I,conjunction(B)).
+
+                :- alt(I), alt(I,conjunction(B)) :
+                        alt(I,output(solution(C,V),B)), solution(C,V).
+            """
+
+        program = self.domain_program
+        program += self.puzzle_gen_program
+        program += self.puzzle_constraints_program
+        program += self.solution_program
+        program += essential_solution_program
+        program += glue_program
+
+        if reified_solution_programs:
+            control.load("meta-alt.lp")
+        control.add("base", [], program)
+        control.ground([("base", [])])
+        if verbose:
+            print("Done grounding..")
+
+        # Find and yield answer sets
+        control.configuration.solve.models = num_models
+        control.configuration.solve.opt_mode = "optN"
+
+        check_program = self.domain_program
+        check_program += self.solution_program
+        check_program += essential_solution_program
+
+        def symbol_renamer(symbol):
+            if symbol == "solution":
+                return "other_solution"
+            return symbol
+
+        check_ast = []
+        parse_string(check_program, lambda ast:
+            check_ast.append(rename_symbolic_atoms(ast, symbol_renamer))
+        )
+
+        glue_program = """
+            solutions_differ :- cell(C), solution(C,V1),
+                other_solution(C,V2), V1 != V2.
+            :- not solutions_differ.
+        """
+        parse_string(glue_program,
+            check_ast.append
+        )
+
+        control.register_propagator(CheckPropagator(check_ast))
+
+        if self.solving_timeout:
+            with control.solve(
+                async_=True,
+                on_model=lambda model: self.interpret_model(model, verbose),
+            ) as handle:
+                finished = handle.wait(self.solving_timeout)
+                if verbose and not finished:
+                    print("\nStopped after solving timeout..")
+                else:
+                    print()
+
+        if verbose:
+            time = control.statistics['summary']['times']['total']
+            print(f"Solving time: {time:.2f} seconds\n")
+
+
+class Checker:
+    """
+    TODO
+    """
+    _ctl: Control
+    _map: List[Tuple[int, int]]
+
+    def __init__(self):
+        self._ctl = Control()
+        self._map = []
+
+    def backend(self) -> Backend:
+        """
+        Return the backend of the underlying solver.
+        """
+        return self._ctl.backend()
+
+    def add(self, guess_lit: int, check_lit: int):
+        """
+        Map the given solver literal to the corresponding program literal in
+        the checker.
+        """
+        self._map.append((guess_lit, check_lit))
+
+    def ground(self, check_ast):
+        """
+        Ground the check program.
+        """
+        # self._ctl.add("base", [], check_program)
+        # self._ctl.ground([("base", [])])
+        with ProgramBuilder(self._ctl) as bld:
+            for stm in check_ast:
+                bld.add(stm)
+        self._ctl.ground([("base", [])])
+
+    def check(self, control: PropagateControl) -> bool:
+        """
+        Return true if the check program is unsatisfiable w.r.t. to the atoms
+        of the guess program.
+
+        The truth values of the atoms of the guess program are stored in the
+        assignment of the given control object.
+        """
+        assignment = control.assignment
+
+        assumptions = []
+        for guess_lit, check_lit in self._map:
+            guess_truth = assignment.value(guess_lit)
+            assumptions.append(check_lit if guess_truth else -check_lit)
+
+        ret = cast(SolveResult, self._ctl.solve(assumptions))
+        if ret.unsatisfiable is not None:
+            return ret.unsatisfiable
+
+        raise RuntimeError("search interrupted")
+
+
+class CheckPropagator(Propagator):
+    """
+    TODO
+    """
+    _checkers: List[Checker]
+    _gluelits: List[int]
+
+    def __init__(self, check_ast):
+        self._checkers = []
+        self._check_ast = check_ast
+        self._gluelits = []
+
+    def init(self, init: PropagateInit):
+        """
+        Initialize the solvers for the check programs.
+        """
+        # we need a checker for each thread (to be able to solve in parallel)
+        for _ in range(init.number_of_threads):
+            checker = Checker()
+            self._checkers.append(checker)
+
+            with checker.backend() as backend:
+                for atom in init.symbolic_atoms:
+
+                    if (atom.symbol.name not in ["solution", "puzzle", "guessed_number"]):
+                        continue
+
+                    self._gluelits.append(init.solver_literal(atom.literal))
+
+                    guess_lit = init.solver_literal(atom.literal)
+                    guess_truth = init.assignment.value(guess_lit)
+
+                    # ignore false atoms
+                    if guess_truth is False:
+                        continue
+
+                    check_lit = backend.add_atom(atom.symbol)
+
+                    # fix true atoms
+                    if guess_truth is True:
+                        backend.add_rule([check_lit], [])
+
+                    # add a choice rule for unknow atoms and add them to the
+                    # mapping table of the checker
+                    else:
+                        backend.add_rule([check_lit], [], True)
+                        checker.add(guess_lit, check_lit)
+
+            checker.ground(self._check_ast)
+
+
+    def check(self, control: PropagateControl):
+        """
+        Check total assignments.
+        """
+        assignment = control.assignment
+        checker = self._checkers[control.thread_id]
+
+        if not checker.check(control):
+
+            conflict = []
+
+            # for level in range(1, assignment.decision_level+1):
+            #     conflict.append(-assignment.decision(level))
+
+            for lit in self._gluelits:
+                conflict.append(-lit if assignment.is_true(lit) else lit)
+
+            control.add_clause(conflict)
+
 
 enc_library = {
     #
